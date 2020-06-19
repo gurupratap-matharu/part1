a4a4e5c8ced256e5695f3879449bf46559012794
Gurupratap Matharu <gurupratap.matharu@gmail.com>
2020-06-19 15:40:15 -0300

    new functionality added and ready for deploy

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+        {text}
+    </button>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Display = ({ text, value }) => (
+    <div>{text}: {value}</div>
+)
+
+const App = () => {
+    // save clicks of each button to own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+
+    const incrementGood = () => {
+        setGood(good + 1)
+    }
+    const incrementNeutral = () => {
+        setNeutral(neutral + 1)
+    }
+    const incrementBad = () => {
+        setBad(bad + 1)
+    }
+    return (
+        <div>
+            <h1>Give Feedback</h1>
+            <Button onClick={incrementGood} text='Good' />
+            <Button onClick={incrementNeutral} text='Neutral' />
+            <Button onClick={incrementBad} text='Bad' />
+            <h1>Statistics</h1>
+            <Display text='Good' value={good} />
+            <Display text='Neutral' value={neutral} />
+            <Display text='Bad' value={bad} />
+            <Display text='All' value={good + neutral + bad} />
+            <Display text='Average' value={(good - bad) / (good + neutral + bad)} />
+            <Display text='Positive' value={good / (good + neutral + bad)} />
+
+        </div>
+    )
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
